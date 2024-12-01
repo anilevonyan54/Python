2afdbcb1ffb291614b09fc51ed104a21ecdcc57a
Ani Levonyan <anilevonyan54@gmail.com>
2024-12-01 22:58:41 +0400

    Create main.py

---
+++ Homework/Lecture 12/main.py
@@ -0,0 +1,6 @@
+# Create a file that will contain 100 lines, each line will contain  20 random  numbers. 
+# Read all the file lines and using map function convert the line into integer array.
+# Using filter function filter the numbers which are > 40 
+# Write the data back to file
+# Read the same file as a generetor(use yield to achieve that)
+# Create decorator that will measure the function execution time 
