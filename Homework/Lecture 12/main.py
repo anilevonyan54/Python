4f752d93043fd12a546f4f5c10d1f2cb80e2355e
Ani Levonyan <anilevonyan54@gmail.com>
2024-12-02 17:09:56 +0400

    homework update

--- Homework/Lecture 12/main.py
+++ Homework/Lecture 12/main.py
@@ -4,3 +4,54 @@
 # Write the data back to file
 # Read the same file as a generetor(use yield to achieve that)
 # Create decorator that will measure the function execution time 
+
+
+import time
+
+def create_numbers_file(filename):
+    with open(filename, 'w') as f:
+        for i in range(100):  
+            line = " ".join(str((num * (i + 3)) % 101 + 1) for num in range(1, 21)) 
+            f.write(line + "\n")
+
+def read_and_process_file(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        processed_lines = []
+        for line in lines:
+            numbers = list(map(int, line.split()))
+            filtered_numbers = list(filter(lambda x: x > 40, numbers))
+            processed_lines.append(filtered_numbers)
+        return processed_lines
+
+def write_filtered_data_to_file(filename, processed_lines):
+    with open(filename, 'w') as f:
+        for line in processed_lines:
+            f.write(" ".join(str(num) for num in line) + "\n")
+
+def read_file_as_generator(filename):
+    with open(filename, 'r') as f:
+        for line in f:
+            yield list(map(int, line.split())) 
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Execution time of {func.__name__}: {end_time - start_time} seconds")
+        return result
+    return wrapper
+
+@measure_time
+def process_file(filename):
+    create_numbers_file(filename)
+    processed_lines = read_and_process_file(filename)
+    write_filtered_data_to_file(filename, processed_lines)
+
+filename = "numbers.txt"
+
+process_file(filename)
+
+for line in read_file_as_generator(filename):
+    print(line)
